b07b81360dd38aef137b715d9fc20b50d34d0efa
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-09-05 17:50:53 +0300

    home update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Box width="400px" sx={{ width: { xl: "1488" } }} m="auto">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/gym_exersise" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
       <Footer />
